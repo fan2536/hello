7ef21ee4a2b3469d6d245085661a30fb963bd182
fan2536 <b604142865@163.com>
2018-04-17 21:55:58 +0800

    提交了qweasdsa

---
+++ src/cn/bdqn/Hello.java
@@ -0,0 +1,24 @@
+package cn.bdqn;
+
+public class Hello {
+
+
+    private  Integer a;
+    private  StringBuffer sb;
+
+    public static void main(String[] args) {
+
+        System.out.print("asdasd");
+
+
+    }
+    
+
+    public Integer getA() {
+        return a;
+    }
+
+    public void setA(Integer a) {
+        this.a = a;
+    }
+}
---
+++ src/cn/bdqn/demo1.java
@@ -0,0 +1,14 @@
+package cn.bdqn;
+
+public class demo1 {
+
+    public static void main(String[] args) {
+
+        System.out.print("你好");
+
+
+
+    }
+
+
+}
